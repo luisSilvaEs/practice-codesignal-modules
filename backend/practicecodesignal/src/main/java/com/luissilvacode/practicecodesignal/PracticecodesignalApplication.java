e2b8e9f9d7e2df267eb882ba4513e1ff7f31c175
Luis Silva <siel_alb@hotmail.com>
2026-03-09 15:26:05 -0600

    feat: set up frontend and backend scaffolding for both services

---
+++ backend/practicecodesignal/src/main/java/com/luissilvacode/practicecodesignal/PracticecodesignalApplication.java
@@ -0,0 +1,13 @@
+package com.luissilvacode.practicecodesignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticecodesignalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticecodesignalApplication.class, args);
+	}
+
+}
---
+++ backend/practicecodesignal/src/test/java/com/luissilvacode/practicecodesignal/PracticecodesignalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luissilvacode.practicecodesignal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticecodesignalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
